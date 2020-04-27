5d4d789ba0996ca2a3791015d22922a37a6fb3a5
Robin Elfrink <robin@15augustus.nl>
2020-04-27 07:50:38 +0200

    Add vlan information.

--- gs1200-exporter.js
+++ gs1200-exporter.js
@@ -22,11 +22,15 @@ const num_ports_gauge = new Gauge({
     help: 'Number of ports. Mainly a placeholder for system information.',
     labelNames: ['model', 'firmware', 'ip', 'mac', 'loop']
 });
-
+const num_vlans_gauge = new Gauge({
+    name: 'gs1200_num_vlans',
+    help: 'Number of configured vlans.',
+    labelNames: ['vlans']
+})
 const speed_gauge = new Gauge({
     name: 'gs1200_speed',
     help: 'Port speed in Mbps.',
-    labelNames: ['port', 'status', 'loop']
+    labelNames: ['port', 'status', 'loop', 'pvlan', 'vlans']
 });
 const tx_gauge = new Gauge({
     name: 'gs1200_packets_tx',
@@ -42,7 +46,8 @@ const rx_gauge = new Gauge({
 async function getData() {
     let scripts = {
         'system_data.js': 'system',
-        'link_data.js': 'link'
+        'link_data.js': 'link',
+        'VLAN_1Q_List_data.js': 'vlan'
     };
     let data = {};
     let promise = Promise.resolve();
@@ -69,6 +74,7 @@ async function getData() {
 }
 
 async function getMetrics() {
+    register.resetMetrics();
     await getData()
     .then(data => {
         num_ports_gauge.set({
@@ -81,10 +87,33 @@ async function getMetrics() {
         for (var i=0; i<parseInt(data.system.Max_port); i++) {
             let port = 'port '+(i+1);
             let status = data.link.portstatus[i];
-            speed_gauge.set({port: port, status: data.link.portstatus[i], loop: data.system.loop_status[i]}, parseInt(data.link.speed[i])*1000*1000);
-            tx_gauge.set({port: port}, data.link.Stats[i][1]+data.link.Stats[i][2]+data.link.Stats[i][3]);
-            rx_gauge.set({port: port}, data.link.Stats[i][6]+data.link.Stats[i][7]+data.link.Stats[i][8]+data.link.Stats[i][10]);
+            let pvlan = 0;
+            let vlans = [];
+            for (var j=0; j<data.vlan.qvlans.length; j++) {
+                if ((data.vlan.qvlans[j][1] >> i) & 1) {
+                    if ((data.vlan.qvlans[j][2] >> i) & 1)
+                        vlans.push(parseInt(data.vlan.qvlans[j][0])).toString();
+                    else
+                        pvlan = parseInt(data.vlan.qvlans[j][0]).toString();
+                }
+            }
+            speed_gauge.set({
+                port: port,
+                status: data.link.portstatus[i],
+                loop: data.system.loop_status[i],
+                pvlan: pvlan,
+                vlans: vlans.join(',')
+            }, parseInt(data.link.speed[i])*1000*1000);
+            tx_gauge.set({
+                port: port
+            }, data.link.Stats[i][1]+data.link.Stats[i][2]+data.link.Stats[i][3]);
+            rx_gauge.set({
+                port: port
+            }, data.link.Stats[i][6]+data.link.Stats[i][7]+data.link.Stats[i][8]+data.link.Stats[i][10]);
         }
+        num_vlans_gauge.set({
+            vlans: data.vlan.qvlans.map(vlan => { return parseInt(vlan[0]).toString(); }).join(',')
+        }, data.vlan.qvlans.length);
     })
     .catch(error => {
         console.log(error);
