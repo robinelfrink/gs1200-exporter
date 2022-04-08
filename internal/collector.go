aee393489a947821d4a0972e36ec7ac096a5e99e
Robin Elfrink <robin@15augustus.nl>
2022-04-08 15:00:33 +0200

    Split and refactor collector and exporter code.
    *  move collector and exporter code to internal/
    *  use only one http.Client for the whole process lifetime
    *  improve error handling

---
+++ internal/collector.go
@@ -0,0 +1,252 @@
+package internal
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/robertkrimen/otto"
+)
+
+var client http.Client
+var vm *otto.Otto
+var systemData SystemData
+var portData []PortData
+
+type Collector struct {
+	address  string
+	password string
+}
+
+type SystemData struct {
+	Max_port    int64
+	model_name  string
+	sys_fmw_ver string
+	sys_IP      string
+	sys_MAC     string
+	loop        string
+	vlans       []string
+}
+
+type PortStats struct {
+	rx float64
+	tx float64
+}
+
+type PortData struct {
+	name        string
+	loop_status string
+	portstatus  string
+	speed       int
+	stats       PortStats
+	pvlan       string
+	vlans       []string
+}
+
+func GS1200Collector(address string, password string) (*Collector, error) {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+	client = http.Client{
+		Jar: jar,
+		Transport: &http.Transport{
+			DisableKeepAlives: true,
+			MaxIdleConns:      5,
+		},
+	}
+	collector := &Collector{
+		address:  address,
+		password: password,
+	}
+	return collector, nil
+}
+
+func (c *Collector) GetValue(name string) otto.Value {
+	value, _ := vm.Get(name)
+	return value
+}
+
+func (c *Collector) GetFloat(name string) float64 {
+	value, _ := c.GetValue(name).ToFloat()
+	return value
+}
+
+func (c *Collector) GetString(name string) string {
+	value, _ := c.GetValue(name).ToString()
+	return value
+}
+
+func (c *Collector) GetInt(name string) int {
+	value, _ := c.GetValue(name).ToInteger()
+	return int(value)
+}
+
+func (c *Collector) GetArrayOfString(name string) []string {
+	export, _ := c.GetValue(name).Export()
+	return export.([]string)
+}
+
+func (c *Collector) GetArrayOfArrayOfString(name string) [][]string {
+	export, _ := c.GetValue(name).Export()
+	return export.([][]string)
+}
+
+func (c *Collector) GetArrayOfArrayOfInterface(name string) [][]interface{} {
+	export, _ := c.GetValue(name).Export()
+	return export.([][]interface{})
+}
+
+func (c *Collector) Collect() (*SystemData, *[]PortData, error) {
+	vm = otto.New()
+
+	// We need system data to determine how to log in
+	err := c.FetchAndParse("system_data.js")
+	if err != nil {
+		return nil, nil, err
+	}
+	systemData = SystemData{
+		Max_port:    int64(c.GetInt("Max_port")),
+		model_name:  c.GetString("model_name"),
+		sys_fmw_ver: c.GetString("sys_fmw_ver"),
+		sys_IP:      c.GetString("sys_IP"),
+		sys_MAC:     c.GetString("sys_MAC"),
+		loop:        c.GetString("loop"),
+	}
+	loop_status := c.GetArrayOfString("loop_status")
+
+	// Login
+	err = c.Login()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Fetch and parse the javascript files containing all the data.
+	c.FetchAndParse("link_data.js")
+	portstatus := c.GetArrayOfString("portstatus")
+	speed := c.GetArrayOfString("speed")
+	stats := c.GetArrayOfArrayOfInterface("Stats")
+	c.FetchAndParse("VLAN_1Q_List_data.js")
+	vlans := c.GetArrayOfArrayOfString("qvlans")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Clear the session.
+	c.Logout()
+
+	// Report number of configured vlans.
+	for _, vlan := range vlans {
+		systemData.vlans = append(systemData.vlans, vlan[0])
+	}
+
+	// Loop over ports
+	portData = make([]PortData, systemData.Max_port)
+	for i := range portData {
+		portData[i].name = "port " + strconv.Itoa(i+1)
+		portData[i].loop_status = loop_status[i]
+		portData[i].portstatus = portstatus[i]
+		portData[i].pvlan = "0"
+		// Loop over configured vlans.
+		for _, vlan := range vlans {
+			// Parse vlan flags
+			flag1, _ := strconv.ParseInt(strings.Replace(vlan[1], "0x", "", -1), 16, 64)
+			if (flag1>>(i))&1 > 0 {
+				// Current vlan is connected to current port.
+				flag2, _ := strconv.ParseInt(strings.Replace(vlan[2], "0x", "", -1), 16, 64)
+				if (flag2>>(i))&1 > 0 {
+					// Tagged
+					portData[i].vlans = append(portData[i].vlans, vlan[0])
+				} else {
+					// Untagged
+					portData[i].pvlan = vlan[0]
+				}
+			}
+		}
+
+		// Port speed seems to always be "[num] Mbps".
+		portData[i].speed, _ = strconv.Atoi(strings.ReplaceAll(speed[i], " Mbps", ""))
+
+		// Sent/received traffic has a weird structure. This is what Zyxel's
+		// code does:
+		//
+		//   tx = Stats[k][1]+Stats[k][2]+Stats[k][3];
+		//   rx = Stats[k][6]+Stats[k][7]+Stats[k][8]+Stats[k][10];
+		//   tx = parseFloat(tx).toLocaleString(); //Divide the numbers with commas
+		//   rx = parseFloat(rx).toLocaleString();
+		portData[i].stats.tx = float64(stats[i][1].(int64) + stats[i][2].(int64) + stats[i][3].(int64))
+		portData[i].stats.rx = float64(stats[i][6].(int64) + stats[i][7].(int64) + stats[i][8].(int64) + stats[i][10].(int64))
+	}
+
+	return &systemData, &portData, nil
+}
+
+func (c *Collector) FetchAndParse(filename string) error {
+	resp, err := client.Get("http://" + c.address + "/" + filename)
+	if err != nil {
+		c.Logout()
+		return err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		c.Logout()
+		return err
+	}
+	vm.Run(string(body))
+	return nil
+}
+
+func (c *Collector) EncryptPassword(password string) string {
+	const letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(password)
+	result := ""
+	for i := 0; i <= len(bytes); i++ {
+		result = result + string(letters[rand.Intn(len(letters))])
+		if i < len(bytes) {
+			result = result + string(rune(int(bytes[i])-len(bytes)))
+		}
+	}
+	return result
+}
+
+func (c *Collector) Login() error {
+	// Log in on the GS1200. If another user is logged on the device
+	// will simply send an empty response, making it impossible
+	// to do proper error-handling.
+	pass := c.password
+	r, _ := regexp.Compile(`^V(\d+\.\d+)\([A-Z]+\.(\d+)\)[A-Z]\d$`)
+	match := r.FindStringSubmatch(systemData.sys_fmw_ver)
+	version, _ := strconv.ParseFloat(match[1], 64)
+	revision, _ := strconv.Atoi(match[2])
+	if len(match) < 1 || (version >= 2 && revision >= 1) {
+		pass = c.EncryptPassword(c.password)
+	}
+	resp, err := client.PostForm("http://"+c.address+"/login.cgi",
+		url.Values{"password": {pass}},
+	)
+	if err != nil {
+		// Even though logging in failed, try to log out, clearing the
+		// session.
+		c.Logout()
+		return err
+	}
+	io.Copy(ioutil.Discard, resp.Body)
+	return nil
+}
+
+func (c *Collector) Logout() {
+	resp, err := client.Get("http://" + c.address + "/logout.html")
+	if err != nil {
+		fmt.Println("Warning:", err)
+	}
+	io.Copy(ioutil.Discard, resp.Body)
+	defer resp.Body.Close()
+}
---
+++ internal/exporter.go
@@ -0,0 +1,91 @@
+package internal
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	namespace = "gs1200"
+)
+
+var (
+	num_ports_metric = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "num_ports"),
+		"Number of ports. Mainly a placeholder for system information.",
+		[]string{"model", "firmware", "ip", "mac", "loop"}, nil)
+	num_vlans_metric = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "num_vlans"),
+		"Number of configured vlans.",
+		[]string{"vlans"}, nil)
+	speed_metric = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "speed"),
+		"Port speed in Mbps.",
+		[]string{"port", "status", "loop", "pvlan", "vlans"}, nil)
+	tx_metric = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "packets_tx"),
+		"Number of packets transmitted.",
+		[]string{"port"}, nil)
+	rx_metric = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "packets_rx"),
+		"Number of packets received.",
+		[]string{"port"}, nil)
+)
+
+type Exporter struct {
+	port      string
+	collector Collector
+}
+
+func GS1200Exporter(collector Collector, port string) *Exporter {
+	return &Exporter{
+		collector: collector,
+		port:      port,
+	}
+}
+
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- num_ports_metric
+	ch <- num_vlans_metric
+	ch <- speed_metric
+	ch <- tx_metric
+	ch <- rx_metric
+}
+
+func (e *Exporter) Run() {
+	prometheus.MustRegister(e)
+	http.Handle("/metrics", promhttp.Handler())
+	fmt.Println("Listening for requests on port", e.port)
+	log.Fatal(http.ListenAndServe(":"+string(e.port), nil))
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	systemData, portData, err := e.collector.Collect()
+	if err != nil {
+		fmt.Println("Collect error:", err)
+		return
+	}
+
+	ch <- prometheus.MustNewConstMetric(num_ports_metric, prometheus.GaugeValue,
+		float64(systemData.Max_port), systemData.model_name, systemData.sys_fmw_ver,
+		systemData.sys_IP, systemData.sys_MAC, systemData.loop)
+
+	ch <- prometheus.MustNewConstMetric(num_vlans_metric, prometheus.GaugeValue,
+		float64(len(systemData.vlans)), strings.Join(systemData.vlans, ","))
+
+	for _, port := range *portData {
+		ch <- prometheus.MustNewConstMetric(speed_metric, prometheus.GaugeValue,
+			float64(port.speed), port.name, port.portstatus, port.loop_status,
+			port.pvlan, strings.Join(port.vlans, ","))
+		ch <- prometheus.MustNewConstMetric(rx_metric, prometheus.GaugeValue,
+			port.stats.rx, port.name)
+		ch <- prometheus.MustNewConstMetric(tx_metric, prometheus.GaugeValue,
+			port.stats.tx, port.name)
+	}
+
+}
