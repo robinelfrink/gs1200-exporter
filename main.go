537fb59bc94f0c41b0380a5eefae0c9fd9561cba
Robin Elfrink <robin@15augustus.nl>
2022-04-24 09:28:19 +0200

    feat: add version information to the compiled binaries

--- main.go
+++ main.go
@@ -8,16 +8,24 @@ import (
 )
 
 var (
+	Version = "development"
+
 	listenPort = flag.String("port", "9934",
 		"Port on which to expose metrics.")
 	gs1200Address = flag.String("address", "192.168.1.3",
 		"IP address or hostname of the GS1200")
 	gs1200Password = flag.String("password", "********",
 		"Password to log on to the GS1200")
+	versionFlag = flag.Bool("version", false,
+		"Show gs1200-exporter version")
 )
 
 func main() {
 	flag.Parse()
+	if *versionFlag {
+		fmt.Println("gs1200-exporter", Version)
+		os.Exit(0)
+	}
 	collector, err := gs1200.GS1200Collector(
 		getEnv("GS1200_ADDRESS", *gs1200Address),
 		getEnv("GS1200_PASSWORD", *gs1200Password),
